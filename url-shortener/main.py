22303a777da1ebb293fff63c81852eec0fe5b35b
mi42195 <aptraev.mv@gmail.com>
2025-05-02 19:20:06 +0500

    feat: implement GET / endpoint returning greeting and link to /docs

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
-
-
-app = FastAPI(
-    title="URL Shortener",
+from fastapi import (
+    FastAPI,
+    Request,
 )
+
+
+app = FastAPI(title="URL Shortener")
+
+
+@app.get("/")
+def get_root(request: Request, name: str = "World"):
+    docs_path = str(
+        request.url.replace(
+            path="/docs",
+            query="",
+        )
+    )
+
+    return {"message": f"Hello {name}", "docs_path": docs_path}
