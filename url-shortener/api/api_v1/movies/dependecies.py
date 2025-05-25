d137b658d51d719fc3f0afd8a0791d1f71cb6d5e
mi42195 <aptraev.mv@gmail.com>
2025-05-25 15:37:19 +0500

    refactor(auth): changed API token transmission from Header to HTTPBearer

--- url-shortener/api/api_v1/movies/dependecies.py
+++ url-shortener/api/api_v1/movies/dependecies.py
@@ -1,8 +1,10 @@
 import logging
 from typing import Annotated
 
+from fastapi.security import HTTPAuthorizationCredentials
 from fastapi import HTTPException, BackgroundTasks
-from fastapi.params import Query, Header
+from fastapi.security import HTTPBearer
+from fastapi.params import Depends
 from starlette import status
 from starlette.requests import Request
 
@@ -50,17 +52,31 @@ def save_storage_state(
         background_tasks.add_task(movie_storage.save_movie)
 
 
+static_api_token = HTTPBearer(
+    scheme_name="Static API token",
+    description="Your **api token** for developer portal [read more](#) ",
+    auto_error=False,
+)
+
+
 def required_api_token_for_unsave_methods(
     request: Request,
     api_token: Annotated[
-        str,
-        Header(alias="x-auth-token"),
-    ] = "",
+        HTTPAuthorizationCredentials | None,
+        Depends(static_api_token),
+    ] = None,
 ):
+    log.info("API token %s", api_token)
     if request.method not in UNSAVE_METHODS:
         return
 
-    if api_token not in API_TOKENS:
+    if api_token.credentials is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required",
+        )
+
+    if api_token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect api-token",
