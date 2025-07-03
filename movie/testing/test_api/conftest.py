71167b68f7ab6a3bf59dd3d1bc9314fd9dab91cb
mi42195 <aptraev.mv@gmail.com>
2025-07-03 23:20:51 +0500

    test: test for the creation of the film through the api

---
+++ movie/testing/test_api/conftest.py
@@ -0,0 +1,28 @@
+from collections.abc import Generator
+
+import pytest
+from starlette.testclient import TestClient
+
+from api.api_v1.auth.services import redis_tokens
+from main import app
+
+
+@pytest.fixture()
+def client() -> Generator[TestClient]:
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def auth_token() -> Generator[str]:
+    token = redis_tokens.generate_and_save_token()
+    yield token
+    redis_tokens.delete_token(token)
+
+
+@pytest.fixture(scope="module")
+def auth_client(auth_token) -> Generator[TestClient]:
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    with TestClient(app) as client:
+        client.headers.update(headers=headers)
+        yield client
---
+++ movie/testing/test_api/test_api_v1/test_movies/test_views/test_list_views/test_create.py
@@ -0,0 +1,31 @@
+import random
+import string
+from datetime import date
+
+from starlette import status
+from starlette.testclient import TestClient
+
+from main import app
+from schemas.muvies import CreateMovies
+
+
+def test_create_movie(auth_client: TestClient) -> None:
+    url = app.url_path_for("create_movie")
+    data: dict[str, str] = CreateMovies(
+        title="test_title",
+        description="test description",
+        release_year=date(2025, 10, 10),
+        director="test director",
+        slug="".join(random.choices(string.ascii_lowercase, k=10)),
+    ).model_dump(mode="json")
+    response = auth_client.post(url=url, json=data)
+    response_data = response.json()
+
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+    assert data["title"] == response_data["title"], response.text
+    assert data["description"] == response_data["description"], response.text
+    assert data["release_year"] == response_data["release_year"], response.text
+    assert data["director"] == response_data["director"], response.text
+    assert data["slug"] == response_data["slug"], response.text
+
+""
