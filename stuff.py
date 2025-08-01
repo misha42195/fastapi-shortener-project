00b944324a27abca83a31f137493ed81840d563e
mi42195 <aptraev.mv@gmail.com>
2025-08-01 09:38:28 +0500

    update staff

--- stuff.py
+++ stuff.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     redis.set("foo", "bar")
     print("foo", redis.get("foo"))
     print("spam", redis.get("spam"))
-    print("foo", redis.getdel("foo"))
+    print("eggs", redis.getdel("eggs"))
