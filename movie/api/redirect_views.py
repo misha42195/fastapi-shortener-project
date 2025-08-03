3beb0bf66f986125a40621ccdaddbd6af9faef50
mi42195 <aptraev.mv@gmail.com>
2025-08-03 18:42:24 +0500

    update pre-commit file

--- movie/api/redirect_views.py
+++ movie/api/redirect_views.py
@@ -1,20 +1,6 @@
-
 from fastapi import APIRouter
 
 router = APIRouter(
     prefix="/r",
     tags=["Redirect"],
 )
-
-
-# @router.get("/{slug}")
-# @router.get("/{slug}/")
-# def redirect_short_url(
-#     url: Annotated[
-#         ShortenedUrl,
-#         Depends(prefetch_short_url),
-#     ],
-# ):
-#     return RedirectResponse(
-#         url=url.target_url,
-#     )
