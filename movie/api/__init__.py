8ae1e6b2d21f27ecca3cafe80657af0f330264a2
mi42195 <aptraev.mv@gmail.com>
2025-08-03 18:47:09 +0500

    broken file

--- movie/api/__init__.py
+++ movie/api/__init__.py
@@ -1,7 +1,1 @@
-__all__ = ("router",)
-from fastapi import APIRouter
-
-from api.api_v1 import router as api_v1_router
-
-router = APIRouter(prefix="/api")
-router.include_router(api_v1_router)
+print("hi")
