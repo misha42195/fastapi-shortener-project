21a0764e59edb44cee65c4fbdf9825f437dc91eb
mi42195 <aptraev.mv@gmail.com>
2025-10-31 15:09:18 +0500

    show inline slug field error if movie with this slug already exists

--- movie/rest/movies/create_views.py
+++ movie/rest/movies/create_views.py
@@ -1,10 +1,13 @@
 from typing import Annotated, Any
 
 from fastapi import APIRouter, Form
+from starlette import status
 from starlette.requests import Request
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, RedirectResponse
 
+from dependencies.movies import GetMovieStorage
 from schemas.muvies import CreateMovies
+from storage.movies.exeptions import MovieAlreadyExistsError
 from templating import templates
 
 router = APIRouter(
@@ -32,12 +35,33 @@ def get_page_create_view(
 @router.post(
     path="/",
     name="movies:create",
+    response_model=None,
 )
 def create_movie(
-    movie_create: Annotated[
-        CreateMovies,
-        Form(),
-    ],
-) -> dict[str, str]:
-
-    return movie_create.model_dump(mode="json")
+    request: Request,
+    movie_create: Annotated[CreateMovies, Form()],
+    storage: GetMovieStorage,
+) -> RedirectResponse | HTMLResponse:
+    try:
+        storage.create_raise_already_exists(movie_in=movie_create)
+    except MovieAlreadyExistsError:
+        errors = {"slug": f"Movie with slug {movie_create.slug!r} already exist"}
+    else:
+        return RedirectResponse(
+            url=request.url_for("movies:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+    context: dict[str, Any] = {}
+    model_schema = CreateMovies.model_json_schema()
+    context.update(
+        model_schema=model_schema,
+        errors=errors,
+        form_validated=True,
+        form_data=movie_create,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="movies/create.html",
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        context=context,
+    )
